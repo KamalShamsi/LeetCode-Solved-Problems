6961773ded2afa656dbb416ec6ad6f69ccc5edd5
Kamaluddin Shamsi <Kamaluddin.Shamsi@gmail.com>
2023-09-22 20:24:31 -0700

    Time: 47 ms (82.03%), Space: 41.3 MB (95.34%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.js
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.js
@@ -3,13 +3,43 @@
  * @return {number}
  */
 var findMin = function(nums) {
-    let left = 0;
-    let right = nums.length-1;
-
-    while( left < right ){
-        let midPoint = Math.floor((left + right)/2);
-        if(nums[midPoint] > nums[right]) left = midPoint+1;
-        else right = midPoint;
+    if (!nums || nums.length === 0) {
+        return null;
     }
-    return nums[left];
+
+    let low = 0, high = nums.length - 1;
+
+    // If list has one element, return that element
+    if (nums.length === 1) {
+        return nums[0];
+    }
+
+    // If the last element is greater than the first element, then no rotation
+    if (nums[high] > nums[0]) {
+        return nums[0];
+    }
+
+    // Binary search way
+    while (low <= high) {
+        const mid = Math.floor((low + high) / 2);
+
+        // If the mid element is greater than its next element, then mid + 1 is pivot
+        if (nums[mid] > nums[mid + 1]) {
+            return nums[mid + 1];
+        }
+
+        // If the mid element is lesser than its previous element, then mid element is pivot
+        if (nums[mid] < nums[mid - 1]) {
+            return nums[mid];
+        }
+        
+        // Decide whether to go left or right. If middle element is greater than the 0th element, this means the smalles value is on the right
+        if (nums[mid] > nums[0]) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+
+    return -1; // Default return, in case of an error
 };
