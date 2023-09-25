59204d7c7f53078b5e74d89615ca2a30808fbfa3
Kamaluddin Shamsi <Kamaluddin.Shamsi@gmail.com>
2023-09-24 22:28:52 -0700

    Time: 91 ms (56.56%), Space: 46 MB (89.52%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.js
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.js
@@ -1,35 +1,46 @@
-var findMedianSortedArrays = function(nums1, nums2) {
-    // Ensure nums1 is the smaller array.
+function findMedianSortedArrays(nums1, nums2) {
+    // Always make sure nums1 is the smaller array.
     if (nums1.length > nums2.length) {
         [nums1, nums2] = [nums2, nums1];
     }
-    
-    const x = nums1.length;
-    const y = nums2.length;
-    let low = 0, high = x;
-    
-    while (low <= high) {
-        const partitionX = Math.floor((low + high) / 2);
-        const partitionY = Math.floor((x + y + 1) / 2) - partitionX;
-        
-        const maxX = partitionX === 0 ? Number.MIN_SAFE_INTEGER : nums1[partitionX - 1];
-        const minX = partitionX === x ? Number.MAX_SAFE_INTEGER : nums1[partitionX];
-        
-        const maxY = partitionY === 0 ? Number.MIN_SAFE_INTEGER : nums2[partitionY - 1];
-        const minY = partitionY === y ? Number.MAX_SAFE_INTEGER : nums2[partitionY];
-        
-        if (maxX <= minY && maxY <= minX) {
-            if ((x + y) % 2 === 0) {
-                return (Math.max(maxX, maxY) + Math.min(minX, minY)) / 2;
-            } else {
-                return Math.max(maxX, maxY);
-            }
-        } else if (maxX > minY) {
-            high = partitionX - 1;
+
+    const m = nums1.length;
+    const n = nums2.length;
+    let imin = 0, imax = m, halfLen = Math.floor((m + n + 1) / 2);
+
+    while (imin <= imax) {
+        const i = Math.floor((imin + imax) / 2);
+        const j = halfLen - i;
+
+        // Adjusting the partition in nums1 to the right.
+        if (i < m && nums1[i] < nums2[j - 1]) {
+            imin = i + 1;
+        }
+        // Adjusting the partition in nums1 to the left.
+        else if (i > 0 && nums1[i - 1] > nums2[j]) {
+            imax = i - 1;
         } else {
-            low = partitionX + 1;
+            // Found the correct partition.
+            let maxOfLeft;
+            if (i === 0) { maxOfLeft = nums2[j - 1]; }
+            else if (j === 0) { maxOfLeft = nums1[i - 1]; }
+            else { maxOfLeft = Math.max(nums1[i - 1], nums2[j - 1]); }
+
+            // If odd number of elements, return max of left.
+            if ((m + n) % 2 === 1) {
+                return maxOfLeft;
+            }
+
+            // If even number of elements, return average of max of left and min of right.
+            let minOfRight;
+            if (i === m) { minOfRight = nums2[j]; }
+            else if (j === n) { minOfRight = nums1[i]; }
+            else { minOfRight = Math.min(nums1[i], nums2[j]); }
+
+            return (maxOfLeft + minOfRight) / 2.0;
         }
     }
-    
-    throw new Error("Input arrays are not sorted.");
-};
+
+    // Return 0.0 if no solution exists (though this shouldn't happen with valid input).
+    return 0.0;
+}
