743f2f43aff5c98a989241e2921dafae0a12dcb2
Kamaluddin Shamsi <Kamaluddin.Shamsi@gmail.com>
2023-09-27 22:40:38 -0700

    Time: 72 ms (57.12%), Space: 42.6 MB (64.31%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {number}
+ */
+var characterReplacement = function(s, k) {
+    let left = 0, right = 0, maxCharCount = 0, maxLength = 0;
+    const freqMap = {};
+
+    for(right = 0; right < s.length; right++) {
+        // Increase the count of current character
+        freqMap[s[right]] = (freqMap[s[right]] || 0) + 1;
+
+        // Update maxCharCount with the frequency of the current character
+        maxCharCount = Math.max(maxCharCount, freqMap[s[right]]);
+
+        // Check if the current window is valid
+        if (right - left + 1 - maxCharCount > k) {
+            freqMap[s[left]]--; // Decrease the count of the character that is being removed
+            left++; // Move the left pointer of the windows
+        }
+        maxLength = Math.max(maxLength, right - left + 1);
+    }
+    return maxLength;
+};
