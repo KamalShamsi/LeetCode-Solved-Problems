46744e08a4edf3b5d8150f4dd2e45e1d3286d03b
Kamaluddin Shamsi <Kamaluddin.Shamsi@gmail.com>
2023-10-04 20:48:49 -0700

    Time: 71 ms (61.61%), Space: 51.7 MB (29.37%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -1,16 +1,17 @@
-function maxProfit(prices) {
-    let minPrice = Number.MAX_VALUE;
-    let maxProfit = 0;
-
-    for (let price of prices) {
-        if (price < minPrice) {
-            // Update the minimum price when a lower price is found
-            minPrice = price;
-        } else if (price - minPrice > maxProfit) {
-            // Update the maximum profit if the potential profit is higher
-            maxProfit = price - minPrice;
-        }
-    }
-
-    return maxProfit;
-}
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+   let buy = prices[0] 
+   let maxProfit = 0;
+   for(let i=0; i<prices.length; i++) {
+     if(prices[i]-buy > maxProfit) {
+         maxProfit = prices[i]-buy
+     }
+     if(prices[i] < buy) {
+         buy = prices[i]
+     }
+   }
+   return maxProfit
+};
