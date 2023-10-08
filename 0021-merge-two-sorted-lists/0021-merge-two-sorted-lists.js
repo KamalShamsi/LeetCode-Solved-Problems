78a09051c616faab426329db7b4ccb345fd9aaa9
Kamaluddin Shamsi <Kamaluddin.Shamsi@gmail.com>
2023-10-07 23:56:05 -0700

    Time: 39 ms (99.73%), Space: 43.9 MB (74.31%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
@@ -1,31 +1,35 @@
-/**
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
- */
-/**
- * @param {ListNode} list1
- * @param {ListNode} list2
- * @return {ListNode}
- */
+function mergeTwoLists(firstList, secondList) {
+    let firstListPointer = firstList;
+    let secondListPointer = secondList;
 
-var mergeTwoLists = function(l1, l2) {
-    let tempNode = new ListNode(0, null);
-    let currentNode = tempNode;
-    
-    while (l1 && l2) {
-        if(l1.val < l2.val) {
-            currentNode.next = l1;
-            l1 = l1.next
+    let newListPointer = new ListNode();
+    const linkedListPointer = newListPointer;
+
+    while(firstListPointer && secondListPointer) {
+        if(firstListPointer.val < secondListPointer.val) {
+            newListPointer.next = firstListPointer;
+            firstListPointer = firstListPointer.next;
         } else {
-            currentNode.next = l2;
-            l2 = l2.next
+            newListPointer.next = secondListPointer;
+            secondListPointer = secondListPointer.next;
         }
-        currentNode = currentNode.next;
+
+        newListPointer = newListPointer.next;
     }
-    currentNode.next = l1 || l2;
-    
-    return tempNode.next;
-};
+
+    while(firstListPointer) {
+        newListPointer.next = firstListPointer;
+        firstListPointer = firstListPointer.next;
+
+        newListPointer = newListPointer.next;
+    }
+
+    while(secondListPointer) {
+        newListPointer.next = secondListPointer;
+        secondListPointer = secondListPointer.next;
+
+        newListPointer = newListPointer.next;
+    }
+
+    return linkedListPointer.next;
+}
