b0d8b072f3450fbd878d6cf1a4e8292db2c9ff31
Kamaluddin Shamsi <Kamaluddin.Shamsi@gmail.com>
2023-10-09 23:40:51 -0700

    Time: 62 ms (37.37%), Space: 43.4 MB (94.86%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
@@ -1,35 +1,18 @@
-function mergeTwoLists(firstList, secondList) {
-    let firstListPointer = firstList;
-    let secondListPointer = secondList;
-
-    let newListPointer = new ListNode();
-    const linkedListPointer = newListPointer;
-
-    while(firstListPointer && secondListPointer) {
-        if(firstListPointer.val < secondListPointer.val) {
-            newListPointer.next = firstListPointer;
-            firstListPointer = firstListPointer.next;
+var mergeTwoLists = function(l1, l2) {
+    let tempNode = new ListNode(0, null);
+    let currentNode = tempNode;
+    
+    while (l1 && l2) {
+        if(l1.val < l2.val) {
+            currentNode.next = l1;
+            l1 = l1.next
         } else {
-            newListPointer.next = secondListPointer;
-            secondListPointer = secondListPointer.next;
+            currentNode.next = l2;
+            l2 = l2.next
         }
-
-        newListPointer = newListPointer.next;
+        currentNode = currentNode.next;
     }
-
-    while(firstListPointer) {
-        newListPointer.next = firstListPointer;
-        firstListPointer = firstListPointer.next;
-
-        newListPointer = newListPointer.next;
-    }
-
-    while(secondListPointer) {
-        newListPointer.next = secondListPointer;
-        secondListPointer = secondListPointer.next;
-
-        newListPointer = newListPointer.next;
-    }
-
-    return linkedListPointer.next;
-}
+    currentNode.next = l1 || l2;
+    
+    return tempNode.next;
+};
