d562518f308b330a6f3429d281571113d4c43005
Kamaluddin Shamsi <Kamaluddin.Shamsi@gmail.com>
2023-10-09 23:56:04 -0700

    Time: 75 ms (5.09%), Space: 44 MB (56.07%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
@@ -1,10 +1,25 @@
-const mergeTwoLists = (l1, l2) => {
-  const dummy = { next: null };
-  let cur = dummy;
-	while (l1 && l2) {
-		l1.val < l2.val ? (cur.next = l1, l1 = l1.next) : (cur.next = l2, l2 = l2.next);
-		cur = cur.next;
-	}
-	cur.next = l1 || l2;
-	return dummy.next;
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+
+var mergeTwoLists = function(list1, list2) {
+    if (list1 == null) return list2;
+    if (list2 == null) return list1;
+
+    if(list1.val < list2.val){
+        list1.next = mergeTwoLists(list1.next, list2)
+        return list1
+    } else {
+        list2.next = mergeTwoLists(list1, list2.next)
+        return list2
+    }
 };
