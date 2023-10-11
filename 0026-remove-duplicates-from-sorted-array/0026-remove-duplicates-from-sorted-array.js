30a224d982585893a7923e95e9fac8ab85673ea9
Kamaluddin Shamsi <Kamaluddin.Shamsi@gmail.com>
2023-10-10 23:50:48 -0700

    Time: 55 ms (89.35%), Space: 44.1 MB (91.27%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -1,9 +1,6 @@
-const removeDuplicates = (nums) => {
-    if (nums.length === 0) return 0;
-    let i = 0;
-    for (let j = 1; j < nums.length; j++) {
-        if (nums[j] === nums[i]) continue; // If the elements are equal then continue else copy the unique element by then to "i+1" position
-        nums[++i] = nums[j];
-    }
-    return i + 1;
-}
+var removeDuplicates = function (nums) {
+      let set = new Set(nums);
+      nums.length = 0;
+      nums.push(...set.values())
+      return set.size;
+};
