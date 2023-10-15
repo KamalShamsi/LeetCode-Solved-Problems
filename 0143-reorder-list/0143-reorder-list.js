d6834463062ed8726f9adc144eec6cbc223e0961
Kamaluddin Shamsi <Kamaluddin.Shamsi@gmail.com>
2023-10-14 23:39:12 -0700

    Time: 86 ms (26.02%), Space: 49.8 MB (74.05%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.js
+++ 0143-reorder-list/0143-reorder-list.js
@@ -1,69 +1,38 @@
-/**
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
- */
-/**
- * @param {ListNode} head
- * @return {void} Do not return anything, modify head in-place instead.
- */
 var reorderList = function (head) {
-  if (head === null) {
-    return;
-  }
-  const secondHalf = split(head);
-  const reversedSecondHalf = reverseLinkedList(secondHalf);
-  // after split(), the head only has first half nodes
-  mergeTwoLists(head, reversedSecondHalf);
+	let slow = head
+	let fast = head
+
+	//find the middle of the list
+	while (fast.next !== null && fast.next.next !== null) {
+		slow = slow.next
+		fast = fast.next.next
+	}
+
+	//separate the lists into two
+	let prev = null
+	let curr = slow.next
+	slow.next = null
+	//reverse the second list
+	while (curr != null) {
+		const next = curr.next
+		curr.next = prev
+		prev = curr
+		curr = next
+	}
+
+	//merge the two lists together
+	let list1 = head
+	let list2 = prev
+
+	while (list2 !== null) {
+		//set the list 1 next pointer to list2 and go to the next node in list1
+		const next1 = list1.next
+		list1.next = list2
+		list1 = next1
+
+		//set the list 2 next pointer to list1 and go to the next node in list2
+		const next2 = list2.next
+		list2.next = list1
+		list2 = next2
+	}
 };
-
-/**
- * Use two pointers to find the middle of a linked list: fast and slow.
- * The fast pointer moves twice as fast as the slow pointer.
- * When the fast pointer reach the end of the list (or second to end),
- * it has "pulled" the slow pointer to the middle.
- */
-function split(head) {
-  let slow = head,
-    fast = head;
-  while (fast.next && fast.next.next) {
-    slow = slow.next;
-    fast = fast.next.next;
-  }
-
-  const secondHalf = slow.next;
-  slow.next = null;
-
-  return secondHalf;
-}
-
-function reverseLinkedList(head) {
-  let prev = null;
-  // exit loop when reaching the end of list
-  while (head !== null) {
-    const next = head.next;
-    head.next = prev;
-    prev = head;
-    head = next;
-  }
-  return prev;
-}
-
-/**
- * list1 will always longer or equal length with list2.
- * head1 is the main head, so it will jump between two lists in zigzag mode
- */
-function mergeTwoLists(list1, list2) {
-  while (list2 !== null) {
-    let nxt1 = list1.next;
-    let nxt2 = list2.next;
-
-    list1.next = list2; // at this point, list1 will be [head1, head2+tail2]
-    list1 = nxt1; // we abandoned nodes of tail1, that's why we have to assign it to the next value immediately, so in the next step, we point back to tail1
-
-    list2.next = list1;
-    list2 = nxt2;
-  }
-}
