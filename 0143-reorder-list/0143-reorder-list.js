6e6f5fbfc830fcee6cc6f738249f8ee44fdae93f
Kamaluddin Shamsi <Kamaluddin.Shamsi@gmail.com>
2023-10-14 23:36:02 -0700

    Time: 76 ms (68.79%), Space: 49.8 MB (74.05%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.js
+++ 0143-reorder-list/0143-reorder-list.js
@@ -9,36 +9,32 @@
  * @param {ListNode} head
  * @return {void} Do not return anything, modify head in-place instead.
  */
-const reorderList = (head) => {
-    // First find the middle of the list 
-    let mid = head, tail = head;
-    while (tail && tail.next) {
-        mid = mid.next;
-        tail = tail.next.next
+var reorderList = function(head) {
+
+    let next = head.next;
+    const stack = [head];
+    
+    while(next) {
+    
+        stack.push(next);
+        next = next.next;
     }
 
-    // Reverse it until the middle
-    let [p, c, n] = [null, mid, null]
-    while(c) {
-        n = c.next
-        c.next = p
-        p = c
-        c = n
+    let l = 0;
+    let r = stack.length - 1;
+
+    let lastProcessed = null;
+
+    while(l <= r) {
+        if (lastProcessed) {
+            lastProcessed.next = stack[l];
+        }
+        stack[l].next = stack[r];
+        lastProcessed = stack[r];
+
+        l++;
+        r--;      
     }
-    // setup for the final step, mainly setting up two pointers we 
-    // will use to make sure the reorder works
-    c = p;
-    tail = c;
-    let end = tail.next;
-    c = head, n = c.next;
-    // Go one at a time reordering the list 
-    while(n && end) {
-        c.next = tail;
-        tail.next = n;
-        c = n;
-        n = n.next;
-        tail = end;
-        end = end.next;
-    }
-    return head;
+
+    lastProcessed.next = null;
 };
