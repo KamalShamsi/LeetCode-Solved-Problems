137b86f9571da2aa9d0c37ec261b203a325cc5b8
Kamaluddin Shamsi <Kamaluddin.Shamsi@gmail.com>
2023-10-14 23:38:46 -0700

    Time: 79 ms (56.03%), Space: 49.7 MB (81.43%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.js
+++ 0143-reorder-list/0143-reorder-list.js
@@ -9,40 +9,61 @@
  * @param {ListNode} head
  * @return {void} Do not return anything, modify head in-place instead.
  */
-var reorderList = function(head) {
-    const nodes = [];
-
-    let node = head;
-
-    while (node) {
-        nodes.push(node);
-
-        node = node.next;
-    }
-
-    let left = 0;
-    let right = nodes.length - 1;
-    let prevNode = null
-
-    while (left <= right) {
-        const leftNode = nodes[left];
-        const rightNode = nodes[right];
-
-        if (prevNode) {
-            prevNode.next = leftNode;
-        }
-
-        if (leftNode !== rightNode) {
-            leftNode.next = rightNode;
-        }
-
-        prevNode = rightNode;
-
-        left++;
-        right--;
-    }
-
-    prevNode.next = null;
-
-    return nodes[0];
+var reorderList = function (head) {
+  if (head === null) {
+    return;
+  }
+  const secondHalf = split(head);
+  const reversedSecondHalf = reverseLinkedList(secondHalf);
+  // after split(), the head only has first half nodes
+  mergeTwoLists(head, reversedSecondHalf);
 };
+
+/**
+ * Use two pointers to find the middle of a linked list: fast and slow.
+ * The fast pointer moves twice as fast as the slow pointer.
+ * When the fast pointer reach the end of the list (or second to end),
+ * it has "pulled" the slow pointer to the middle.
+ */
+function split(head) {
+  let slow = head,
+    fast = head;
+  while (fast.next && fast.next.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+
+  const secondHalf = slow.next;
+  slow.next = null;
+
+  return secondHalf;
+}
+
+function reverseLinkedList(head) {
+  let prev = null;
+  // exit loop when reaching the end of list
+  while (head !== null) {
+    const next = head.next;
+    head.next = prev;
+    prev = head;
+    head = next;
+  }
+  return prev;
+}
+
+/**
+ * list1 will always longer or equal length with list2.
+ * head1 is the main head, so it will jump between two lists in zigzag mode
+ */
+function mergeTwoLists(list1, list2) {
+  while (list2 !== null) {
+    let nxt1 = list1.next;
+    let nxt2 = list2.next;
+
+    list1.next = list2; // at this point, list1 will be [head1, head2+tail2]
+    list1 = nxt1; // we abandoned nodes of tail1, that's why we have to assign it to the next value immediately, so in the next step, we point back to tail1
+
+    list2.next = list1;
+    list2 = nxt2;
+  }
+}
