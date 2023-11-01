ada454bd4eb06add572480e6559fe7cbc7b7d99e
Kamaluddin Shamsi <Kamaluddin.Shamsi@gmail.com>
2023-10-31 23:11:31 -0700

    Time: 64 ms (10.91%), Space: 44.5 MB (29.62%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.js
+++ 0206-reverse-linked-list/0206-reverse-linked-list.js
@@ -1,7 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
 var reverseList = function(head) {
-    let [prev, current] = [null, head]
-    while(current) {
-        [current.next, prev, current] = [prev, current, current.next]
+    // let node = new ListNode(0)
+    let arr = []
+    while(head){
+        arr.push(head.val)
+        head = head.next
     }
-    return prev
-}
+    let reversed = arr.reverse()
+
+    let node1 = new ListNode(0)
+    let node2 = node1
+
+    for(let item of reversed){
+        node1.next = new ListNode(item)
+        node1 = node1.next
+    }
+
+    return node2.next
+};
