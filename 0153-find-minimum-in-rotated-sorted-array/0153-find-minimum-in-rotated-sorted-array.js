10266ebdfedec71848257df8a69500c75fb00cd0
Kamaluddin Shamsi <Kamaluddin.Shamsi@gmail.com>
2023-11-03 23:39:02 -0700

    Time: 40 ms (97.3%), Space: 42.1 MB (42.13%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.js
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.js
@@ -1,45 +1,10 @@
-/**
- * @param {number[]} nums
- * @return {number}
- */
-var findMin = function(nums) {
-    if (!nums || nums.length === 0) {
-        return null;
-    }
-
-    let low = 0, high = nums.length - 1;
-
-    // If list has one element, return that element
-    if (nums.length === 1) {
-        return nums[0];
-    }
-
-    // If the last element is greater than the first element, then no rotation
-    if (nums[high] > nums[0]) {
-        return nums[0];
-    }
-
-    // Binary search way
-    while (low <= high) {
-        const mid = Math.floor((low + high) / 2);
-
-        // If the mid element is greater than its next element, then mid + 1 is pivot
-        if (nums[mid] > nums[mid + 1]) {
-            return nums[mid + 1];
-        }
-
-        // If the mid element is lesser than its previous element, then mid element is pivot
-        if (nums[mid] < nums[mid - 1]) {
-            return nums[mid];
-        }
-        
-        // Decide whether to go left or right. If middle element is greater than the 0th element, this means the smalles value is on the right
-        if (nums[mid] > nums[0]) {
-            low = mid + 1;
-        } else {
-            high = mid - 1;
-        }
-    }
-
-    return -1; // Default return, in case of an error
+const findMin = (nums) => {
+  let l = 0;
+  let r = nums.length - 1;
+  while (l < r) {
+    const m = ~~((l + r) / 2);
+    if (nums[m] > nums[r]) l = m + 1;
+    else r = m;
+  }
+  return nums[l];
 };
