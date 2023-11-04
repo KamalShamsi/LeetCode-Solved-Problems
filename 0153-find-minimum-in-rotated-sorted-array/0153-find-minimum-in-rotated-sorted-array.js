edee2be941c261ae6d861e61ddc9c20835608b26
Kamaluddin Shamsi <Kamaluddin.Shamsi@gmail.com>
2023-11-03 23:39:50 -0700

    Time: 49 ms (69.62%), Space: 42 MB (42.13%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.js
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.js
@@ -1,4 +1,30 @@
-var findMin = function(nums) {
-  nums.sort((a, b) => a - b);
-  return nums[0];
-};
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMin = function (nums) {
+    let left = 0
+    let right = nums.length - 1
+
+    let min = nums[0]
+
+    let mid = Math.floor((left + right) / 2)
+
+    while (left <= right) {
+        if (min > nums[mid]) {
+            min = nums[mid]
+        }
+
+        if (nums[mid] > nums[left]) {
+            left = mid
+        } else if (nums[mid] < nums[left]) {
+            right = mid
+        } else {
+            left++
+        }
+
+        mid = Math.floor((left + right) / 2)
+    }
+
+    return min
+}
