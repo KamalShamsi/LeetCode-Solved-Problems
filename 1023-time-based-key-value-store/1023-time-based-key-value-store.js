f27b33dcc9f971b36b6f77fb7f1c57856dc6ba3e
Kamaluddin Shamsi <Kamaluddin.Shamsi@gmail.com>
2023-11-04 23:34:36 -0700

    Time: 320 ms (91.72%), Space: 99.6 MB (47.1%) - LeetHub

--- 1023-time-based-key-value-store/1023-time-based-key-value-store.js
+++ 1023-time-based-key-value-store/1023-time-based-key-value-store.js
@@ -1,62 +1,27 @@
-var TimeMap = function () {
-    this.map = {}
-
-    return this
-}
-
-/**
- * @param {string} key
- * @param {string} value
- * @param {number} timestamp
- * @return {void}
- */
-TimeMap.prototype.set = function (key, value, timestamp) {
-    if (this.map[key]) {
-        this.map[key].push([value, timestamp])
-    } else {
-        this.map[key] = [[value, timestamp]]
+class TimeMap {
+    constructor() {
+        this.data = new Map();
     }
 
-    return null
-}
-
-/**
- * @param {string} key
- * @param {number} timestamp
- * @return {string}
- */
-TimeMap.prototype.get = function (key, timestamp) {
-    const current = this.map[key]
-
-    if (!current) {
-        return ''
-    }
-
-    let l = 0
-    let r = current.length - 1
-
-    let result = ''
-
-    while (l <= r) {
-        let mid = Math.floor((l + r) / 2)
-
-        if (current[mid][1] > timestamp) {
-            r = mid - 1
-            continue
+    set = (key, value, timestamp) => {
+        if (!this.data.has(key)) {
+            this.data.set(key, []);
         }
+        this.data.get(key).push({ value, timestamp });
+    };
 
-        if (current[mid][1] <= timestamp) {
-            result = current[mid][0]
-            l = mid + 1
+    get = (key, timestamp) => {
+        if (!this.data.has(key)) return "";
+        const values = this.data.get(key);
+        let left = 0;
+        let right = values.length - 1;
+        while (left <= right) {
+            let mid = left + right >>> 1
+            if (values[mid].timestamp <= timestamp) {
+                left = mid + 1;
+            } else right = mid - 1;
         }
-    }
-
-    return result
+        if (right >= 0) return values[right].value;
+        return "";
+    };
 }
-
-/**
- * Your TimeMap object will be instantiated and called as such:
- * var obj = new TimeMap()
- * obj.set(key,value,timestamp)
- * var param_2 = obj.get(key,timestamp)
- */
