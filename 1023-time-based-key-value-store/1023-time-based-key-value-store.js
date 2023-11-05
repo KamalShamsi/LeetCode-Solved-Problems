d884d7ed121f9c510553eca4d122908d63671ab3
Kamaluddin Shamsi <Kamaluddin.Shamsi@gmail.com>
2023-11-04 23:34:45 -0700

    Time: 322 ms (90.37%), Space: 99.7 MB (41.3%) - LeetHub

--- 1023-time-based-key-value-store/1023-time-based-key-value-store.js
+++ 1023-time-based-key-value-store/1023-time-based-key-value-store.js
@@ -1,27 +1,45 @@
-class TimeMap {
-    constructor() {
-        this.data = new Map();
-    }
+ class TimeMap {
+     constructor() {
+         this.map = new Map();
+     }
 
-    set = (key, value, timestamp) => {
-        if (!this.data.has(key)) {
-            this.data.set(key, []);
-        }
-        this.data.get(key).push({ value, timestamp });
-    };
+     set(key, value, timestamp) {
+         if (this.map.has(key)) {
+             const oldVals = this.map.get(key);
+             oldVals.push([value, timestamp]);
+             this.map.set(key, oldVals);
+         } else {
+             this.map.set(key, [[value, timestamp]]);
+         }
+     }
 
-    get = (key, timestamp) => {
-        if (!this.data.has(key)) return "";
-        const values = this.data.get(key);
-        let left = 0;
-        let right = values.length - 1;
-        while (left <= right) {
-            let mid = left + right >>> 1
-            if (values[mid].timestamp <= timestamp) {
-                left = mid + 1;
-            } else right = mid - 1;
-        }
-        if (right >= 0) return values[right].value;
-        return "";
-    };
-}
+     get(key, timestamp) {
+         let result = "";
+         
+         if (this.map.has(key)) {
+             const pairs = this.map.get(key);
+             let left = 0;
+             let right = pairs.length - 1;
+            
+             while (left <= right) {
+                 let middle = Math.floor((left + right) / 2);
+
+                 if (pairs[middle][1] > timestamp) {
+                     right = middle - 1;
+                 } else if (pairs[middle][1] < timestamp) {
+                     left = middle + 1;
+                     result = pairs[middle][0];
+                 } else {
+                     result = pairs[middle][0];
+                     return result;
+                 }
+             }
+
+             if (left > right) {
+                 return result;
+             }
+         } else {
+             return result;
+         }
+     }
+ }
