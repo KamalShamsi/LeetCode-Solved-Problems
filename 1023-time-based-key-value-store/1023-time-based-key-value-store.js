61c3fde1bfda21d23ec984c84a13cd750d5dae5e
Kamaluddin Shamsi <Kamaluddin.Shamsi@gmail.com>
2023-11-04 23:34:55 -0700

    Time: 321 ms (91.1%), Space: 98.4 MB (83.95%) - LeetHub

--- 1023-time-based-key-value-store/1023-time-based-key-value-store.js
+++ 1023-time-based-key-value-store/1023-time-based-key-value-store.js
@@ -1,45 +1,24 @@
- class TimeMap {
-     constructor() {
-         this.map = new Map();
-     }
-
-     set(key, value, timestamp) {
-         if (this.map.has(key)) {
-             const oldVals = this.map.get(key);
-             oldVals.push([value, timestamp]);
-             this.map.set(key, oldVals);
-         } else {
-             this.map.set(key, [[value, timestamp]]);
-         }
-     }
-
-     get(key, timestamp) {
-         let result = "";
-         
-         if (this.map.has(key)) {
-             const pairs = this.map.get(key);
-             let left = 0;
-             let right = pairs.length - 1;
-            
-             while (left <= right) {
-                 let middle = Math.floor((left + right) / 2);
-
-                 if (pairs[middle][1] > timestamp) {
-                     right = middle - 1;
-                 } else if (pairs[middle][1] < timestamp) {
-                     left = middle + 1;
-                     result = pairs[middle][0];
-                 } else {
-                     result = pairs[middle][0];
-                     return result;
-                 }
-             }
-
-             if (left > right) {
-                 return result;
-             }
-         } else {
-             return result;
-         }
-     }
- }
+class TimeMap {
+    constructor() {
+        this.map = new Map()
+    }
+    set(k, v, ts) {
+        if (!this.map.has(k)) this.map.set(k, [])
+        this.map.get(k).push([v, ts])
+    }
+    get(k, ts) {
+        const arr = this.map.get(k) || []
+        return this.#bs(arr, ts)
+    }
+    #bs(arr, ts) {
+        let res = "", l = 0, r = arr.length - 1
+        while (l <= r) {
+            const p = (l + r) >>> 1
+            const [v, t] = arr[p]
+            if (t === ts) return v
+            if (t > ts) r = p - 1
+            else res = v, l = p + 1
+        }
+        return res
+    }
+};
