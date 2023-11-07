7354320028be772bdd6a0067981017318a879705
Kamaluddin Shamsi <Kamaluddin.Shamsi@gmail.com>
2023-11-07 00:00:11 -0800

    Time: 67 ms (83.64%), Space: 47.1 MB (59.62%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -1,21 +1,16 @@
-/**
- * @param {string} s
- * @return {number}
- */
 var lengthOfLongestSubstring = function(s) {
-      let maxLength = 0;
-    let set = new Set();
-    let i = 0;
-    let j = 0;
-    while (i < s.length && j < s.length) {
-        if (!set.has(s[j])) {
-            set.add(s[j]);
-            maxLength = Math.max(maxLength, j - i + 1);
-            j++;
-        } else {
-            set.delete(s[i]);
-            i++;
+    var maxLength = 0
+    var left = 0
+    var charSet = new Set()
+    
+    for (var right = 0; right < s.length; right++){
+        while (charSet.has(s[right])){
+            charSet.delete(s[left])
+            left += 1
         }
+        charSet.add(s[right])
+        maxLength = Math.max(maxLength, right - left + 1)
     }
-    return maxLength;
+    
+    return maxLength
 };
