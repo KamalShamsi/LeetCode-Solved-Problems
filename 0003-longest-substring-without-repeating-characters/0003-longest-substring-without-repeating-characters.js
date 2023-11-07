b840a39b3ca2d92bd57c6a75cfb0669ef1ba6bc1
Kamaluddin Shamsi <Kamaluddin.Shamsi@gmail.com>
2023-11-06 23:59:29 -0800

    Time: 297 ms (13.57%), Space: 48.7 MB (32.77%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -1,19 +1,19 @@
-/**
- * @param {string} s
- * @return {number}
- */
 var lengthOfLongestSubstring = function(s) {
-    const setMap = new Set();
-    let max = 0;
-    let index = 0;
-    for(let i = 0; i<s.length; i++){
-        let key = s[i];
-        while(setMap.has(key)){
-            setMap.delete(s[index]);
-            index++;
+    const pointer1 = {'l': 0, 'r': 1};
+    const pointer2 = {'l': 0, 'r': 1};
+
+    while (pointer2.r <= s.length) {
+        const s1 = s.slice(pointer1.l, pointer1.r);
+        const s2 = s.slice(pointer2.l, pointer2.r);
+        if (new Set(s2.split('')).size < s2.length) { // s2 not unique
+            pointer2.l++;
+        } else if (s2.length > s1.length) { // unique and bigger
+            pointer1.l = pointer2.l;
+            pointer1.r = pointer2.r;
+            pointer2.r++;
+        } else {
+            pointer2.r++;
         }
-        setMap.add(key);
-        max = Math.max(max, setMap.size);
     }
-    return max;
+    return s.slice(pointer1.l, pointer1.r).length;
 };
