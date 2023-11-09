18a6f012a77181f78a2ea794e866aaea463811e2
Kamaluddin Shamsi <Kamaluddin.Shamsi@gmail.com>
2023-11-08 23:24:44 -0800

    Time: 67 ms (71.53%), Space: 42.1 MB (83.06%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.js
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.js
@@ -1,26 +1,22 @@
-/**
- * https://leetcode.com/problems/longest-repeating-character-replacement/
- * Time O(((N + 26) * N) * (M - N)) | Space O(1)
- * @param {string} s
- * @param {number} k
- * @return {number}
- */
-var characterReplacement = function(s, k) {
-    let res = 0;
-    let count = new Map();
-    let l = 0;
+const characterReplacement = (s, k) => {
+  let left = 0;
+  let right = 0;
+  let maxCharCount = 0;
+  const visited = {};
 
-    for (let r = 0; r < s.length; r++) {
-        let len  = r - l + 1
-        count.set(s[r], 1 + (count.get(s[r]) || 0))
+  while (right < s.length) {
+    const char = s[right];
+    visited[char] = visited[char] ? visited[char] + 1 : 1;
 
-        if ((len - Math.max(...count.values())) > k) {
-            count.set(s[l], count.get(s[l]) - 1)
-            l++;
-        }
-        len = r - l + 1;
-        res = Math.max(res, len)
+    if (visited[char] > maxCharCount) maxCharCount = visited[char];
+
+    if (right - left + 1 - maxCharCount > k) {
+      visited[s[left]]--;
+      left++;
     }
 
-    return res;
+    right++;
+  }
+
+  return right - left;
 };
