c4b6e92033c0c7d3035c0790f0b5f79559fbb7e2
Kamaluddin Shamsi <Kamaluddin.Shamsi@gmail.com>
2023-11-12 22:38:05 -0800

    Time: 77 ms (77%), Space: 46.3 MB (53.09%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.js
+++ 0076-minimum-window-substring/0076-minimum-window-substring.js
@@ -1,33 +1,60 @@
-const minWindow = function (s, t) {
-  // Edge case: if either string is empty or s is shorter than t, return an empty string
-  if (!s || !t || s.length < t.length) return "";
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {string}
+ */
+var minWindow = function(s, t) {  
+    if (s.length < t.length) return ''
+    
+    const tMap = t.split('').reduce((acc,el) => {
+       if (!acc[el]) {
+           acc[el] = 1
+       } else {
+           acc[el] ++
+       }
 
-  // Initialize pointers and variables to keep track of the window and result
-  let l = 0, r = 0;
-  let count = 0, minI = s.length + 1, minL = s.length + 1;
+        return acc
+    }, {})
 
-  // Initialize a frequency map for characters in t
-  const freqMap = {};
-  for (const ch of t) freqMap[ch] = (freqMap[ch] || 0) + 1;
+    let formed = 0 
+    const required = Object.keys(tMap).length
+    let i = 0 
+    let j = 0 
 
-  // Expand the window to the right
-  while (r < s.length) {
-    // If the character is needed, decrement its count in the map and increase the count
-    if (freqMap[s[r]]-- >= 1) count += 1;
-    r += 1;
+    const windowCount = {}
 
-    // Check if the window is valid (contains all characters of t)
-    while (count == t.length) {
-      // Update the result if the current window is smaller
-      if (r - l < minL) {
-        minL = r - l;
-        minI = l;
-      }
-      // Try to minimize the window by moving the left pointer to the right
-      if (freqMap[s[l]]++ >= 0) count -= 1;
-      l += 1;
+    let res = [-1, 0, 0]
+
+    while (j < s.length) {
+        if (!windowCount[s[j]]) windowCount[s[j]] = 0 
+        windowCount[s[j]] ++ 
+
+        if (tMap[s[j]] && windowCount[s[j]] === tMap[s[j]]) {
+            formed ++
+        }
+
+        while (i <= j && formed === required) {
+            if (res[0] == -1 || j - i + 1 < res[0]) {
+                res[0] = j - i + 1
+                res[1] = i
+                res[2] = j
+
+                if (res[0] === t.length) return s.slice(res[1], res[2] + 1)
+            }
+
+            windowCount[s[i]]-- 
+
+            if (tMap[s[i]] && windowCount[s[i]] < tMap[s[i]]) {
+                formed --
+            }
+
+            i++
+
+        }
+
+        j++
     }
-  }
-  // Return the minimum window substring
-  return s.slice(minI, minI + minL);
+
+    return res[0] < 0 ? '' : s.slice(res[1], res[2] + 1)
+    
 };
