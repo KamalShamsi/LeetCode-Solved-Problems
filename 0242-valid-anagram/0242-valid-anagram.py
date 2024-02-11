5e51e877658ddfccfca160610852de3437d92d4e
Kamaluddin Shamsi <Kamaluddin.Shamsi@gmail.com>
2024-02-10 20:50:57 -0800

    Time: 44 ms (84.83%), Space: 16.9 MB (68.87%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,16 +1,16 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        # Check if the lengths of the strings are different
         if len(s) != len(t):
             return False
         
-        # Count the frequency of each character in both strings
-        countS = {}
-        countT = {}
-        for char in s:
-            countS[char] = countS.get(char, 0) + 1
-        for char in t:
-            countT[char] = countT.get(char, 0) + 1
+        countS, countT = {}, {}
         
-        # Check if both dictionaries have the same character frequency
-        return countS == countT
+        for i in range (len(s)):
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+            countT[t[i]] = 1 + countT.get(t[i], 0)
+        for c in countS:
+            if countS[c] != countT.get(c, 0):
+                return False
+        
+        return True
+            
