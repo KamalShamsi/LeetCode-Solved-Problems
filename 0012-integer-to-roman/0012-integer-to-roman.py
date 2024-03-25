78db5418305a660670fdbc58499d676f184bd0a4
Kamaluddin Shamsi <Kamaluddin.Shamsi@gmail.com>
2024-03-24 17:19:19 -0700

    Time: 57 ms (13.20%), Space: 16.4 MB (94.64%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -0,0 +1,21 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        # Mapping of Roman numerals to their values
+        roman_numerals = [
+            ("M", 1000), ("CM", 900), ("D", 500), ("CD", 400),
+            ("C", 100), ("XC", 90), ("L", 50), ("XL", 40),
+            ("X", 10), ("IX", 9), ("V", 5), ("IV", 4), ("I", 1)
+        ]
+        
+        # Initialize the result string
+        result = ""
+        
+        # Convert integer to Roman numeral
+        for roman, value in roman_numerals:
+            while num >= value:
+                result += roman
+                num -= value
+                
+        return result
+    
+    
